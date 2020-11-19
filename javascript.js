e2fedab597080da6a4a19fd0fa91b750f9709b26
eliseweir <67879336+eliseweir@users.noreply.github.com>
2020-11-18 21:13:46 -0600

    Add weather fetching functionality

--- javascript.js
+++ javascript.js
@@ -5,15 +5,14 @@ let currentCity = "Memphis";
 let currentState = "TN";
 let currentLat = 35.15;
 let currentLong = -90.05;
-let currentOffset = -21600;
 
 // start the page with weather for Memphis, TN
-update(currentCity, currentState, currentLat, currentLong, currentOffset);
+update(currentCity, currentState, currentLat, currentLong);
 
 // update the weather every 15 minutes
 setInterval(
   function() {
-    update(currentCity, currentState, currentLat, currentLong, currentOffset);
+    update(currentCity, currentState, currentLat, currentLong);
   },
   15*60000);
 
@@ -22,10 +21,10 @@ let form = document.getElementById("form");
 form.addEventListener("submit", getPlace);
 
 // update everything on the page
-function update(city, state, lat, long, offset) {
+function update(city, state, lat, long) {
   displayDate();
   displayLocation(city, state);
-  getWeather(lat, long, offset);
+  getWeather(lat, long);
 }
 
 // fetch the lat/long and time offset for a location,
@@ -52,15 +51,13 @@ function getPlace(event) {
       displayLocation(city, state);
       let lon = data.coord.lon;
       let lat = data.coord.lat;
-      let timeOffset = data.timezone;
-      getWeather(lat, lon, timeOffset);
+      getWeather(lat, lon);
 
       // update current city so that the refresh shows that information
       currentCity = city;
       currentState = state;
       currentLong = lon;
       currentLat = lat;
-      currentOffset = timeOffset;
     }
   });
   
@@ -69,19 +66,6 @@ function getPlace(event) {
   event.preventDefault();
 }
 
-// called by getPlace()
-// fetch the current and daily forecast for a location
-function getWeather(lat, lon, timeOffset) {
-  // fetch the current and daily forecasts
-  fetch(`${URI}onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=${"minutely,hourly,alerts"}&appid=${KEY}`)
-  .then((res) => {
-    return res.json();
-  }).then((data) => {
-    // act on the data
-    currentTemp(data.current);
-    displayForecast(data.daily, timeOffset);
-  });
-}
 
 // called by getPlace()
 // display the current city and state
@@ -92,6 +76,21 @@ function displayLocation(city, state) {
   stateText.textContent = state;
 }
 
+// called by getPlace()
+// fetch the current and daily forecast for a location
+function getWeather(lat, lon) {
+  // fetch the current and daily forecasts
+  fetch(`${URI}onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=${"minutely,hourly,alerts"}&appid=${KEY}`)
+  .then((res) => {
+    return res.json();
+  }).then((data) => {
+    // act on the data
+    currentTemp(data.current);
+    displayForecast(data.daily);
+  });
+}
+
+
 // getPlace() -> getWeather -> currentTemp()
 // display current temperature and feels like for a place
 function currentTemp(current) {
@@ -100,43 +99,75 @@ function currentTemp(current) {
 
   // display current temperature in F
   let tempF = current.temp;
-  temp.textContent = tempF;
+  temp.textContent = Math.round(tempF);
 
   // display feels-like temperature in F
   tempF = current.feels_like;
-  feelsLike.textContent = tempF;
+  feelsLike.textContent = Math.round(tempF);
 }
 
 // getplace() -> getWeather -> displayForecast
 // display the 7-day temperatures for a place
-function displayForecast(daily, timeOffset) {
-  for (let i = 0; i < 7; i++) {
-    displayDaily(daily[i], i, timeOffset);
+function displayForecast(daily) {
+  let currentHigh = document.getElementById("current-high");
+  let currentLow = document.getElementById("current-low");
+
+  let high = daily[0].temp.max;
+  let low = daily[0].temp.min;
+
+  currentHigh.textContent = Math.round(high);
+  currentLow.textContent = Math.round(low);
+
+  for (let i = 1; i < 7; i++) {
+    displayDaily(daily[i], i);
   } 
 }
 
 // getPlace() -> getWeather -> displayForecast -> displayDaily
 // display the one-day forecast for a place
-function displayDaily(cast, index, timeOffset) {
-  let li = document.getElementById(`day-${index}`);
-  let date = li.getElementsByClassName("date")[0];
-  let min = li.getElementsByClassName("min")[0];
-  let max = li.getElementsByClassName("max")[0];
+function displayDaily(cast, index) {
+  let card = document.getElementById(`day-${index}`);
+  let date = card.getElementsByClassName("date")[0];
+  let min = card.getElementsByClassName("min")[0];
+  let max = card.getElementsByClassName("max")[0];
+  let sunrise = card.getElementsByClassName("sunrise")[0];
+  let sunset = card.getElementsByClassName("sunset")[0];
 
-  let currentTime = new Date((cast.dt + timeOffset)*1000);
-  let weekDay = currentTime.toLocaleString("en-US", {weekday: "short"});
-  let month = currentTime.toLocaleString("en-US", {month: "short"});
-  let day = currentTime.toLocaleString("en-US", {day: "numeric"});
+  let currentTime = new Date((cast.dt)*1000);
+  let currentDate = currentTime.toLocaleDateString('en-US', 
+  { weekday: 'long', month: 'numeric', day: 'numeric' }
+  );
+  let sunup = new Date((cast.sunrise)*1000).toLocaleTimeString("en-US",
+  {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true
+  });
+  let sundown = new Date((cast.sunset)*1000).toLocaleTimeString("en-US",
+  {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true
+  });
 
-  date.textContent = `${weekDay}, ${month} ${day}`;
+  date.textContent = currentDate;
   min.textContent = Math.round(cast.temp.min);
   max.textContent = Math.round(cast.temp.max);
+  sunrise.textContent = sunup;
+  sunset.textContent = sundown;
 }
 
 // display the current date and time
 function displayDate() {
   let date = document.getElementById("date");
   var d = new Date();
-  formattedDate = `${d.toDateString()} ${d.getHours()}:${d.getMinutes()}`;
+  formattedDate = d.toLocaleString("en-US" , {
+    weekday: "long",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute:"2-digit",
+    hour12: true
+  });
   date.textContent = formattedDate;
 }
